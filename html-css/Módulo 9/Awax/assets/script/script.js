3bb6ea816abafed65369b3c94973e9eb08c2b200
john cobain <andreygdsn@gmail.com>
2022-10-22 22:44:26 -0300

    our team

--- html-css/Módulo 9/Awax/assets/script/script.js
+++ html-css/Módulo 9/Awax/assets/script/script.js
@@ -16,3 +16,22 @@ function changeSlide(slide){
         document.getElementById('pointer3').classList.add('active');
     }
 }
+
+function changeSlideTeam(slide){
+    const sliders = document.querySelector('.sliders-team');
+
+    const pointers = document.querySelectorAll('.pointer-team');
+
+    pointers.forEach(pointer => pointer.classList.remove('active'));
+
+    if(slide === 1){
+        sliders.style.marginLeft = '0';
+        document.getElementById('pointerT1').classList.add('active');
+    }else if(slide === 2){
+        sliders.style.marginLeft = '-900px';
+        document.getElementById('pointerT2').classList.add('active');
+    }else if(slide === 3){
+        sliders.style.marginLeft = '-1800px';
+        document.getElementById('pointerT3').classList.add('active');
+    }
+}
