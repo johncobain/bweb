1e96658234f05272fa9ebd3fa29b976b41341c0c
john cobain <andreygdsn@gmail.com>
2022-10-25 21:50:29 -0300

    exercicio pt2

--- html-css/Módulo 9/Awax/assets/script/script.js
+++ html-css/Módulo 9/Awax/assets/script/script.js
@@ -54,3 +54,25 @@ function changeSlideClients(slide){
         document.getElementById('pointerC3').classList.add('active');
     }
 }
+
+function changeSlidePlans(slide){
+    const sliders = document.querySelector('.sliders-plans');
+
+    const pointers = document.querySelectorAll('.pointer-plans');
+
+    pointers.forEach(pointer => pointer.classList.remove('active'));
+
+    if(slide === 1){
+        sliders.style.marginLeft = '0';
+        document.getElementById('pointerP1').classList.add('active');
+    }else if(slide === 2){
+        sliders.style.marginLeft = '-1140px';
+        document.getElementById('pointerP2').classList.add('active');
+    }else if(slide === 3){
+        sliders.style.marginLeft = '-2280px';
+        document.getElementById('pointerP3').classList.add('active');
+    }else if(slide === 4){
+        sliders.style.marginLeft = '-3420px';
+        document.getElementById('pointerP4').classList.add('active');
+    }
+}
