827772c647254f3f014c13ccb925445fef41aefd
john cobain <andreygdsn@gmail.com>
2022-11-12 14:34:37 -0300

    introduzindo js

---
+++ javaScript/Módulo 1/assets/js/inserindoJs.js
@@ -0,0 +1,27 @@
+
+//variaveis
+let nome = 'John'; let idade = 17; let maiorIdade = false;
+let nescau = ['leite', 'nescau'];
+let nomeCompleto = {
+    nome: 'John',
+    sobrenome: 'Cobain'
+};
+
+//apresentar no console
+console.log('hello world');
+console.log(`${nome} has ${idade} years`);
+console.log(typeof nomeCompleto);
+
+//exercício #1
+
+//1
+let car = "Ferrari";
+console.log(car);
+
+//2
+let price = 19.99;
+console.log(`R$${price}`);
+
+//3
+let cidade = 'São Paulo';
+console.log(cidade);
